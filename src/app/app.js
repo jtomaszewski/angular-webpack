c9b663ec745dbce5ec3c8a7f09c59db081021c50
Jacek Tomaszewski <jacek@jtom.me>
2016-10-09 22:18:54 +0200

    Add hot reload using hot-app and angular-hot

--- src/app/app.js
+++ src/app/app.js
@@ -1,8 +1,10 @@
 import angular from 'angular';
+import hotAngular from 'angular-hot';
+import {HotApp} from 'hot-app';
 
 import '../style/app.css';
 
-let app = () => {
+const AppDirective = () => {
   return {
     template: require('./app.html'),
     controller: 'AppCtrl',
@@ -18,8 +20,36 @@ class AppCtrl {
 
 const MODULE_NAME = 'app';
 
-angular.module(MODULE_NAME, [])
-  .directive('app', app)
+if (!window.__ng1AppDefined) {
+  hotAngular.module(MODULE_NAME, []);
+  window.__ng1AppDefined = true;
+}
+
+hotAngular.module(MODULE_NAME)
+  .directive('app', AppDirective)
   .controller('AppCtrl', AppCtrl);
+
+const app = window.app = new HotApp({
+  oldApp: window.app,
+  getRootElement: function() { return document.body; },
+  startFn: (app, onStart) => {
+    angular.bootstrap(app.getRootElement(), [MODULE_NAME]);
+    onStart();
+  },
+  stopFn: (app, onStop) => {
+    angular.element(app.getRootElement()).injector().get('$rootScope').$destroy();
+    onStop();
+  }
+});
+app.startOnDOMReady();
+
+// Webpack hot reload support
+if (module.hot) {
+  module.hot.accept();
+
+  module.hot.dispose(() => {
+    app.stop();
+  });
+}
 
 export default MODULE_NAME;
